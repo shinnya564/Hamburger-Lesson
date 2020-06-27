96b1a6ebc5401d8ee9aab977be3b344be1df7e97
shinnya <65529573+shinnya564@users.noreply.github.com>
2020-06-27 17:56:09 +0900

    [finish]

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(function() {
+
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+
+ });
